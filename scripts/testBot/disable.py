ba78cd22e3e2bf68ab2a91734f2e6f37276c239c
renfeiw <renfeiw@ca.ibm.com>
2020-10-21 14:14:57 -0400

    Update testBot disable function
    - use an xml parser to replace the regex match
    - add variation disable
    - add a format function
    
    Signed-off-by: renfeiw <renfeiw@ca.ibm.com>

--- scripts/testBot/disable.py
+++ scripts/testBot/disable.py
@@ -12,7 +12,7 @@
 # limitations under the License.
 ################################################################################
 
-import autoModifier
+import playlistModifier
 import argparse
 import re
 import sys
@@ -21,7 +21,7 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("-m", "--message", required=True, help="exclude message")
     ap.add_argument("-d", "--directory", required=True, help="directory to modify")
-    ap.add_argument("-i", "--issue", required=True, help="issue URL")
+    ap.add_argument("-c", "--comment", required=True, help="comment")
     args = vars(ap.parse_args())
 
     newArgs = {
@@ -31,32 +31,21 @@ def main():
     match = re.match(r"auto ([A-Za-z]+) test (\S+)( in (\S+))?", message)
     if match:
         print(f"{args['message']}\n")
-        mode = match.group(1)
-        test = match.group(2)
         file = "playlist.xml"
         if match.group(3):
             file = match.group(4)
-        newArgs.update({
-            "file": file
-        })
-        if (mode == "exclude") and (file == "playlist.xml"):
-            excludeTestInPlaylist(test, args["issue"], newArgs)
+        if (file == "playlist.xml"):
+            newArgs.update({
+                "mode": match.group(1),
+                "test": match.group(2),
+                "comment": args["comment"],
+                "file": file
+            })
+            playlistModifier.run(newArgs)
         else:
             print("function not supported for now")
     else:
         print(f"unrecognized message: {args['message']}")
-
-
-def excludeTestInPlaylist(test, issue, args):
-    args.update({
-        "find" : fr"(([ \t]*)<\s*testCaseName\s*>\s*{test}\s*<\s*/testCaseName\s*>)",
-        "replace": fr"\1\n\2<disabled>{issue}</disabled>"
-    })
-    rt = autoModifier.run(args)
-    if not rt:
-        print(f"Could not find {test} in {args['file']}!")
-        sys.exit(-1)
-
 
 if __name__ == "__main__":
     main()
