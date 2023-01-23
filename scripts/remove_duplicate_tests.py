731fca6ddf7ac5924ae12c1d11879c9594ce6973
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2023-01-24 01:45:47 +0530

    Add license and XML declaration
    Signed-off-by: ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>

--- scripts/remove_duplicate_tests.py
+++ scripts/remove_duplicate_tests.py
@@ -1,3 +1,17 @@
+################################################################################
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
 import xml.etree.ElementTree as ET
 
 def remove_duplicate_test_tags(xml_file):
@@ -10,8 +24,7 @@ def remove_duplicate_test_tags(xml_file):
             root.remove(test)
         else:
             test_case_names.append(test_case_name)
-    head_strings = '''<?xml version='1.0' encoding='UTF-8'?>
-<!--
+    license = '''<!--
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -25,12 +38,13 @@ def remove_duplicate_test_tags(xml_file):
 # limitations under the License.
 -->
 '''
-    #add head strings to the xml file
-    tree.write(xml_file)
+    #add license comment
+    tree.write(xml_file, encoding='UTF-8', xml_declaration=True)
     with open(xml_file, 'r+') as f:
-        content = f.read()
+        content = f.readlines()
+        content.insert(1, license)
+        content = ''.join(content)
         f.seek(0, 0)
-        f.write(head_strings)
         f.write(content)
 
 if __name__ == '__main__':
