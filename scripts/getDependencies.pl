6c68ac61bac7a8e21266a0a72323734b0184f626
Vedant Borkar <vedantborkar1234@gmail.com>
2023-02-06 19:55:59 +0530

    Update getDependencies.pl (#399)

--- scripts/getDependencies.pl
+++ scripts/getDependencies.pl
@@ -143,13 +143,6 @@ my %base = (
 		shafn => 'jtreg_6_1_1.tar.gz.sha256sum.txt',
 		shaalg => '256'
 	},
-	jtreg_7_1 => {
-		url => 'https://ci.adoptopenjdk.net/job/dependency_pipeline/lastSuccessfulBuild/artifact/jtreg/jtreg-7+1.tar.gz',
-		fname => 'jtreg_7_1.tar.gz',
-		shaurl => 'https://ci.adoptopenjdk.net/job/dependency_pipeline/lastSuccessfulBuild/artifact/jtreg/jtreg-7+1.tar.gz.sha256sum.txt',
-		shafn => 'jtreg_7_1.tar.gz.sha256sum.txt',
-		shaalg => '256'
-	},
 	jtreg_7_1_1_1 => {
 		url => 'https://ci.adoptopenjdk.net/job/dependency_pipeline/lastSuccessfulBuild/artifact/jtreg/jtreg-7.1.1+1.tar.gz',
 		fname => 'jtreg_7_1_1_1.tar.gz',
