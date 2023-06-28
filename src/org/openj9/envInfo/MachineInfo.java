14dba4af37fa01ebe0080698dcfcd622479ecb49
renfeiw <rfwang009@gmail.com>
2023-06-28 15:25:13 -0400

    Fix machineInfo curl validation
    - update curl version command
    - switch machineInfo detection to warning mode
    - processBuilder to use env copied from the parent shell environment
    
    Signed-off-by: renfeiw <rfwang009@gmail.com>

--- src/org/openj9/envInfo/MachineInfo.java
+++ src/org/openj9/envInfo/MachineInfo.java
@@ -61,7 +61,7 @@ public class MachineInfo {
 	public static final String[] ANT_VERSION_CMD = new String[] {"bash", "-c", "ant -version"};
 	public static final String[] MAKE_VERSION_CMD = new String[] {"bash", "-c", "make --version"};
 	public static final String[] PERL_VERSION_CMD = new String[] {"bash", "-c", "perl --version"};
-	public static final String[] CURL_VERSION_CMD = new String[] {"bash", "-c", "curl --version"};
+	public static final String[] CURL_VERSION_CMD = new String[] {"bash", "-c", "curl --version | head -n 1 | awk '{ print $2 }'"};
 
 
 	// Console
@@ -142,11 +142,12 @@ public class MachineInfo {
 		}
 	}
 
-	private boolean validateVersion(String versionName, String actualVersionStr, String requriedVersionStr) {
+	private boolean validateVersion(Info info, String requriedVersionStr) {
 		boolean isValid = true;
+		String actualVersionStr = parseInfo(info.output);
+		ArrayList<Integer> accVer = versionStr2ArrList(actualVersionStr);
+		ArrayList<Integer> reqVer = versionStr2ArrList(requriedVersionStr);
 		try { 
-			ArrayList<Integer> accVer = versionStr2ArrList(actualVersionStr);
-			ArrayList<Integer> reqVer = versionStr2ArrList(requriedVersionStr);
 			int accVerLen = accVer.size();
 			int reqVerLen = reqVer.size();
 			if (accVerLen > reqVerLen) {
@@ -162,11 +163,11 @@ public class MachineInfo {
 				}
 			}
 			if (!isValid) {
-				System.out.println("Error: required " + versionName + ": " + requriedVersionStr + ". Installed version: " + actualVersionStr);
+				System.out.println("Warning: required " + info.name + ": " + requriedVersionStr + ". Output:\n" + info.output);
 			}
 		} catch (NumberFormatException e){
-			System.out.println("Warning: "+ versionName + " information cannot be extracted.");
-			System.out.println(versionName + " output: " + actualVersionStr);
+			System.out.println("Warning: "+ info.name + " information cannot be extracted.");
+			System.out.println(info.name + " output: " + actualVersionStr);
 		}
 		return isValid;
 	}
@@ -175,13 +176,15 @@ public class MachineInfo {
 		boolean valid = true;
 		for (Info info : infoMap.values()) {
 			if (info.req != null) {
-				String version = parseInfo(info.output);
-				valid &= validateVersion(info.name, version, info.req);
+				valid &= validateVersion(info, info.req);
 			}
 		}
-		if (!valid) {
-			System.exit(1);
-		}
+
+		// Do not fail if the check not pass for the build environment.
+		// We need to add an option to toggle the failure or warning mode.
+		// if (!valid) {
+		//	System.exit(1);
+		//}
 	}
 
 	private void getSysInfo() {
