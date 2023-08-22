06d7099c42807301886e2fe6adf2e8a9137dd552
Keith W. Campbell <keithc@ca.ibm.com>
2023-08-22 09:32:49 -0400

    Also download jtreg-7.3.1+1
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- scripts/getDependencies.pl
+++ scripts/getDependencies.pl
@@ -171,13 +171,18 @@ my %base = (
 		shafn => 'jtreg_7_3_1.tar.gz.sha256sum.txt',
 		shaalg => '256'
 	},
-
+	jtreg_7_3_1_1 => {
+		url => 'https://ci.adoptium.net/job/dependency_pipeline/lastSuccessfulBuild/artifact/jtreg/jtreg-7.3.1+1.tar.gz',
+		fname => 'jtreg_7_3_1_1.tar.gz',
+		shaurl => 'https://ci.adoptium.net/job/dependency_pipeline/lastSuccessfulBuild/artifact/jtreg/jtreg-7.3.1+1.tar.gz.sha256sum.txt',
+		shafn => 'jtreg_7_3_1_1.tar.gz.sha256sum.txt',
+		shaalg => '256'
+	},
 	jython => {
 		url => 'https://repo1.maven.org/maven2/org/python/jython-standalone/2.7.2/jython-standalone-2.7.2.jar',
 		fname => 'jython-standalone.jar',
 		sha1 => '15592c29538abd36d15570eda9fa055ed1a618ba'
 	},
-
 	jcstress => {
 		url => 'https://builds.shipilev.net/jcstress/jcstress-tests-all-20220908.jar',
 		fname => 'jcstress-tests-all-20220908.jar',
